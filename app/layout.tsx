99ddee6f56ecb172ef8f441c4b861ed4b066cbf5
junseopsan <junseopsan@gmail.com>
2026-05-29 10:36:42 +0900

    refactor: update layout metadata and formatting
    - Changed the description in the metadata to a Korean phrase.
    - Improved code formatting for better readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Hilly Heally',
-  description: 'HILL YOUR WAY, HEAL YOUR WAY',
-  generator: 'hillyheally.com',
-}
+  title: "Hilly Heally",
+  description: "나만의 지도를 만들고, 함께 움직이는 경험을 만드세요.",
+  generator: "hillyheally.com",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/favicon/favicon.ico" type="image/x-icon" />
+        <link
+          rel="icon"
+          href="/images/favicon/favicon.ico"
+          type="image/x-icon"
+        />
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
